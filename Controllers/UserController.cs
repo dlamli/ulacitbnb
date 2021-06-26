17236d1f0d2323220d477f8f092c3a1887caa970
DLam28 <dlaml838@ulacit.ed.cr>
2021-06-26 13:40:56 -0600

    Add UserController

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -15,7 +15,7 @@ namespace ulacit_bnb.Controllers
     [RoutePrefix("api/user")]
     public class UserController : ApiController
     {
-        //GET
+        //GET GetId
         [HttpGet]
         public IHttpActionResult GetId(int id)
         {
@@ -109,126 +109,133 @@ namespace ulacit_bnb.Controllers
             return Ok(users);
         }
 
-        ////POST
-        //[HttpPost]
-        //public IHttpActionResult Ingresar(User user)
-        //{
-        //    if (habitacion == null)
-        //    {
-        //        return BadRequest("Ingrese los datos necesarios para insertar una habitación.");
-        //    }
-        //    try
-        //    {
-        //        using (SqlConnection sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["RESERVAS"].ConnectionString))
-        //        {
-        //            SqlCommand sqlCommand = new SqlCommand(@"INSERT INTO Habitacion(
-        //                                                          HOT_CODIGO
-        //                                                        , HAB_NUMERO
-        //                                                    	, HAB_CAPACIDAD
-        //                                                    	, HAB_TIPO
-        //                                                    	, HAB_DESCRIPCION
-        //                                                    	, HAB_ESTADO
-        //                                                    	, HAB_PRECIO)
-        //                                                    VALUES(@HOT_CODIGO
-        //                                                        , @HAB_NUMERO
-        //                                                    	, @HAB_CAPACIDAD
-        //                                                    	, @HAB_TIPO
-        //                                                    	, @HAB_DESCRIPCION
-        //                                                    	, @HAB_ESTADO
-        //                                                    	, @HAB_PRECIO)", sqlConnection);
+        //POST
+        [HttpPost]
+        public IHttpActionResult Ingresar(User user)
+        {
+            if (user == null)
+            {
+                return BadRequest("Please enter required fields to create an user.");
+            }
+            try
+            {
+                using (SqlConnection sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["ULACITBnB"].ConnectionString))
+                {
+                    SqlCommand sqlCommand = new SqlCommand(@"INSERT INTO [dbo].[User](
+                                                                  [Use_Name]
+                                                                 ,[Use_LastName]
+                                                                 ,[Use_Identification]
+                                                                 ,[Use_Password]
+                                                                 ,[Use_Email]
+                                                                 ,[Use_Status]
+                                                                 ,[Use_BirthDate]
+                                                                 ,[Use_Phone])
+                                                            VALUES(@Use_Name
+                                                                , @Use_LastName
+                                                            	, @Use_Identification
+                                                            	, @Use_Password
+                                                            	, @Use_Email
+                                                            	, @Use_Status
+                                                            	, @Use_BirthDate
+                                                                , @Use_Phone)", sqlConnection);
 
-        //            sqlCommand.Parameters.AddWithValue("HOT_CODIGO", habitacion.HOT_CODIGO);
-        //            sqlCommand.Parameters.AddWithValue("HAB_NUMERO", habitacion.HAB_NUMERO);
-        //            sqlCommand.Parameters.AddWithValue("HAB_CAPACIDAD", habitacion.HAB_CAPACIDAD);
-        //            sqlCommand.Parameters.AddWithValue("HAB_TIPO", habitacion.HAB_TIPO);
-        //            sqlCommand.Parameters.AddWithValue("HAB_DESCRIPCION", habitacion.HAB_DESCRIPCION);
-        //            sqlCommand.Parameters.AddWithValue("HAB_ESTADO", habitacion.HAB_ESTADO);
-        //            sqlCommand.Parameters.AddWithValue("HAB_PRECIO", habitacion.HAB_PRECIO);
+                    sqlCommand.Parameters.AddWithValue("Use_Name", user.Use_Name);
+                    sqlCommand.Parameters.AddWithValue("Use_LastName", user.Use_LastName);
+                    sqlCommand.Parameters.AddWithValue("Use_Identification", user.Use_Identification);
+                    sqlCommand.Parameters.AddWithValue("Use_Password", user.Use_Password);
+                    sqlCommand.Parameters.AddWithValue("Use_Email", user.Use_Email);
+                    sqlCommand.Parameters.AddWithValue("Use_Status", user.Use_Status);
+                    sqlCommand.Parameters.AddWithValue("Use_BirthDate", user.Use_BirthDate);
+                    sqlCommand.Parameters.AddWithValue("Use_Phone", user.Use_Phone);
 
-        //            sqlConnection.Open();
-        //            int filasAfectadas = sqlCommand.ExecuteNonQuery();
-        //            sqlConnection.Close();
-        //        }
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        return InternalServerError(ex);
-        //    }
-        //    return Ok(habitacion);
-        //}
 
-        ////PUT
-        //[HttpPut]
-        //public IHttpActionResult Actualizar(User user)
-        //{
-        //    if (habitacion == null)
-        //    {
-        //        return BadRequest("La habitación no existe en la base de datos.");
-        //    }
-        //    try
-        //    {
-        //        using (SqlConnection sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["RESERVAS"].ConnectionString))
-        //        {
-        //            SqlCommand sqlCommand = new SqlCommand(@"UPDATE Habitacion
-        //                                                    SET HOT_CODIGO = @HOT_CODIGO
-        //                                                        , HAB_NUMERO = @HAB_NUMERO
-        //                                                    	, HAB_CAPACIDAD = @HAB_CAPACIDAD
-        //                                                    	, HAB_TIPO = @HAB_TIPO
-        //                                                    	, HAB_DESCRIPCION = @HAB_DESCRIPCION
-        //                                                    	, HAB_ESTADO = @HAB_ESTADO
-        //                                                    	, HAB_PRECIO = @HAB_PRECIO
-        //                                                    WHERE HAB_CODIGO = @HAB_CODIGO", sqlConnection);
+                    sqlConnection.Open();
+                    int filasAfectadas = sqlCommand.ExecuteNonQuery();
+                    sqlConnection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+            return Ok(user);
+        }
 
-        //            sqlCommand.Parameters.AddWithValue("HAB_CODIGO", habitacion.HAB_CODIGO);
-        //            sqlCommand.Parameters.AddWithValue("HOT_CODIGO", habitacion.HOT_CODIGO);
-        //            sqlCommand.Parameters.AddWithValue("HAB_NUMERO", habitacion.HAB_NUMERO);
-        //            sqlCommand.Parameters.AddWithValue("HAB_CAPACIDAD", habitacion.HAB_CAPACIDAD);
-        //            sqlCommand.Parameters.AddWithValue("HAB_TIPO", habitacion.HAB_TIPO);
-        //            sqlCommand.Parameters.AddWithValue("HAB_DESCRIPCION", habitacion.HAB_DESCRIPCION);
-        //            sqlCommand.Parameters.AddWithValue("HAB_ESTADO", habitacion.HAB_DESCRIPCION);
-        //            sqlCommand.Parameters.AddWithValue("HAB_PRECIO", habitacion.HAB_PRECIO);
+        //PUT
+        [HttpPut]
+        public IHttpActionResult Actualizar(User user)
+        {
+            if (user == null)
+            {
+                return BadRequest("User not found in database.");
+            }
+            try
+            {
+                using (SqlConnection sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["ULACITBnB"].ConnectionString))
+                {
+                    SqlCommand sqlCommand = new SqlCommand(@"UPDATE [dbo].[User]
+                                                            SET Use_Name = @Use_Name
+                                                                , Use_LastName = @Use_LastName
+                                                            	, Use_Identification = @Use_Identification
+                                                            	, Use_Password = @Use_Password
+                                                            	, Use_Email = @Use_Email
+                                                            	, Use_Status = @Use_Status
+                                                            	, Use_BirthDate = @Use_BirthDate
+                                                                , Use_Phone = @Use_Phone
+                                                            WHERE Use_ID = @Use_ID", sqlConnection);
 
-        //            sqlConnection.Open();
-        //            int filasAfectadas = sqlCommand.ExecuteNonQuery();
-        //            sqlConnection.Close();
-        //        }
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        return InternalServerError(ex);
-        //    }
-        //    return Ok(habitacion);
-        //}
+                    sqlCommand.Parameters.AddWithValue("Use_ID", user.Use_ID);
+                    sqlCommand.Parameters.AddWithValue("Use_Name", user.Use_Name);
+                    sqlCommand.Parameters.AddWithValue("Use_LastName", user.Use_LastName);
+                    sqlCommand.Parameters.AddWithValue("Use_Identification", user.Use_Identification);
+                    sqlCommand.Parameters.AddWithValue("Use_Password", user.Use_Password);
+                    sqlCommand.Parameters.AddWithValue("Use_Email", user.Use_Email);
+                    sqlCommand.Parameters.AddWithValue("Use_Status", user.Use_Status);
+                    sqlCommand.Parameters.AddWithValue("Use_BirthDate", user.Use_BirthDate);
+                    sqlCommand.Parameters.AddWithValue("Use_Phone", user.Use_Phone);
 
-        ////DELETE
-        //[HttpDelete]
-        //public IHttpActionResult Eliminar(int id)
-        //{
 
-        //    if (id <= 0)
-        //    {
-        //        return BadRequest($"La habitación con el ID:{id} no existe en la base de datos.");
-        //    }
-        //    try
-        //    {
-        //        using (SqlConnection sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["RESERVAS"].ConnectionString))
-        //        {
-        //            SqlCommand sqlCommand = new SqlCommand(@"DELETE FROM Habitacion
-        //                                                    WHERE HAB_CODIGO = @HAB_CODIGO", sqlConnection);
+                    sqlConnection.Open();
+                    int filasAfectadas = sqlCommand.ExecuteNonQuery();
+                    sqlConnection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+            return Ok(user);
+        }
 
-        //            sqlCommand.Parameters.AddWithValue("@HAB_CODIGO", id);
-        //            sqlConnection.Open();
+        //DELETE
+        [HttpDelete]
+        public IHttpActionResult Eliminar(int id)
+        {
 
-        //            int filasAfectadas = sqlCommand.ExecuteNonQuery();
-        //            sqlConnection.Close();
+            if (id <= 0)
+            {
+                return BadRequest($"Usse with ID:{id} not found in database.");
+            }
+            try
+            {
+                using (SqlConnection sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["ULACITBnB"].ConnectionString))
+                {
+                    SqlCommand sqlCommand = new SqlCommand(@"DELETE FROM [dbo].[User]
+                                                            WHERE Use_ID = @Use_ID", sqlConnection);
 
-        //        }
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        return InternalServerError(ex);
-        //    }
-        //    return Ok(id);
-        //}
+                    sqlCommand.Parameters.AddWithValue("@Use_ID", id);
+                    sqlConnection.Open();
+
+                    int filasAfectadas = sqlCommand.ExecuteNonQuery();
+                    sqlConnection.Close();
+
+                }
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+            return Ok(id);
+        }
     }
 }
