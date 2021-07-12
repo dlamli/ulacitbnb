01af92c47802a5840e5f6b40f217e0ca39fdc33d
DLam28 <dlaml838@ulacit.ed.cr>
2021-07-12 11:15:09 -0600

    Remove GetConnectionStringByName() and optimization.

--- ulacitbnb/db/ConnectionString.cs
+++ ulacitbnb/db/ConnectionString.cs
@@ -9,25 +9,12 @@ namespace ulacit_bnb.db
 {
     public class ConnectionString
     {
-        static readonly string db_conn_string = GetConnectionStringByName("UlacitbnbAzureDB");
+        static readonly string db_conn_string = ConfigurationManager.ConnectionStrings["UlacitbnbAzureDB"].ConnectionString;
 
         public static SqlConnection GetSqlConnection() {
             SqlConnection sqlConn = new SqlConnection(db_conn_string);
             return sqlConn;
         }
 
-        public static  string GetConnectionStringByName(string name)
-        {
-            string conn = null;
-            ConnectionStringSettings settings = ConfigurationManager.ConnectionStrings[name];
-
-            if (settings != null)
-            {
-                    conn = settings.ConnectionString;
-                    Console.WriteLine(conn);
-            }
-
-            return conn;
-        }
     }
 }
