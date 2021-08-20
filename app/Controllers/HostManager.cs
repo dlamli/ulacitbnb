b7df9e531a0b9e557bc3702bfa2c8971348e3fb9
Josue Gabuardi Bautista <josuegabuardi@gmail.com>
2021-08-20 00:00:05 -0600

    Add getList and GetbyId

--- app/Controllers/HostManager.cs
+++ app/Controllers/HostManager.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web;
+using ulacitbnb.Models;
 
 namespace AppUlacitBnB.Controllers
 {
     public class Class1
     {
-        string controllerUrl = "http://localhost:49220/api/customer"
+        string controllerUrl = "http://localhost:49220/api/host";
+
+        HttpClient GetHttpClient(string token)
+        {
+            HttpClient httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Add("Authorization", token);
+            httpClient.DefaultRequestHeaders.Add("Accept", "application/json");
+            return httpClient;
+        }
+
+        public async Task<IEnumerable<Host>>
+
     }
 }
