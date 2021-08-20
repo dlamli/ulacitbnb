9377103f1d91432aeaf817ce7489b3e2a4d6db46
Josue Gabuardi Bautista <josuegabuardi@gmail.com>
2021-08-19 23:51:47 -0600

    Create host manager

---
+++ app/Controllers/HostManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppUlacitBnB.Controllers
+{
+    public class Class1
+    {
+        string controllerUrl = "http://localhost:49220/api/customer"
+    }
+}
