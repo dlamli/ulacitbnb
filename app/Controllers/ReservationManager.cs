812f1d9ed530aa801502252de92362d3e4b4085a
Fabricio Cortes Murillo <fcortesm438@ulacit.ed.cr>
2021-08-20 23:36:23 -0600

    Management created and working

---
+++ app/Controllers/ReservationManager.cs
@@ -0,0 +1,81 @@
+﻿using AppUlacitBnB.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace AppUlacitBnB.Controllers
+{
+    public class ReservationManager
+    {
+        string Url = "http://localhost:49220/api/reservation/";
+
+        HttpClient GetClient(string token)
+        {
+            HttpClient client = new HttpClient();
+
+            client.DefaultRequestHeaders.Add("Authorization", token);
+            client.DefaultRequestHeaders.Add("Accept", "application/json");
+
+            return client;
+        }
+
+        public async Task<IEnumerable<Reservation>> GetReservationList(string token)
+        {
+            HttpClient httpClient = GetClient(token);
+
+            string result = await httpClient.GetStringAsync(Url);
+
+            return JsonConvert.DeserializeObject<IEnumerable<Reservation>>(result);
+        }
+
+        public async Task<Reservation> GetReservation(string token, string code)
+        {
+            HttpClient httpClient = GetClient(token);
+
+            string result = await httpClient.GetStringAsync(string.Concat(Url, code));
+
+            return JsonConvert.DeserializeObject<Reservation>(result);
+        }
+
+        public async Task<Reservation> EnterReservation(Reservation reservation, string token)
+        {
+            HttpClient httpClient = GetClient(token);
+
+            var response = await httpClient.PostAsync(Url, new StringContent(JsonConvert.SerializeObject(reservation), Encoding.UTF8, "application/json"));
+
+            return JsonConvert.DeserializeObject<Reservation>(await response.Content.ReadAsStringAsync());
+        }
+
+        public async Task<Reservation> UpdateReservation(Reservation reservation, string token)
+        {
+            try
+            {
+                HttpClient httpClient = GetClient(token);
+
+                var response = await httpClient.PutAsync(Url, new StringContent(JsonConvert.SerializeObject(reservation), Encoding.UTF8, "application/json"));
+
+                return JsonConvert.DeserializeObject<Reservation>(await response.Content.ReadAsStringAsync());
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+
+        }
+
+        public async Task<string> DeleteReservation(string code, string token)
+        {
+            HttpClient httpClient = GetClient(token);
+
+            var response = await httpClient.DeleteAsync(string.Concat(Url, code));
+
+            return JsonConvert.DeserializeObject<string>(await response.Content.ReadAsStringAsync());
+        }
+    }
+}
---
+++ app/Models/Reservation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppUlacitBnB.Models
+{
+    public class Reservation
+    {
+        public int Res_ID { get; set; }
+        public DateTime Res_StartDate { get; set; }
+        public DateTime Res_ReservationDate { get; set; }
+        public DateTime Res_EndDate { get; set; }
+        public string Res_Status { get; set; }
+        public decimal Res_Quantity { get; set; }
+        public string Res_ResolutionDate { get; set; }
+        public int Res_PaymentID { get; set; }
+        public int Cus_ID { get; set; }
+        public int Roo_ID { get; set; }
+    }
+}
